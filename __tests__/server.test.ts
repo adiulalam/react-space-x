8c81d91df5301ce2a2eea0b9e162ec485026764c
Adiul Alam Adil <adiulalam@gmail.com>
2022-11-06 22:18:51 +0000

    cleanup

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,33 +1,14 @@
-import { gql } from "@apollo/client";
 import { client } from "../src/connection/client";
-
-const TestQuery = gql`
-  query ListLaunches {
-    launches: launchesPast(limit: 1) {
-      id
-      launch_date_utc
-      launch_year
-      mission_name
-      links {
-        mission_patch
-        flickr_images
-      }
-      rocket {
-        rocket_name
-      }
-      ships {
-        id
-        image
-        name
-        year_built
-      }
-    }
-  }
-`;
+import { LaunchQuery } from "../src/connection/query";
 
 const testConnection: any = async () => {
   return await client.query({
-    query: TestQuery,
+    query: LaunchQuery,
+    variables: {
+      offset: 0,
+      limit: 10,
+      mission_name: "",
+    },
   });
 };
 
